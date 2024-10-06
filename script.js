3c2141ddee42d12c33e809ce38cc3065278d0666
ferfleitass <fleitasbaez55@gmail.com>
2024-10-06 17:37:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll suave en los enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
